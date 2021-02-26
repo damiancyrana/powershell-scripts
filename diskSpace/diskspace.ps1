1b0c9449421ed6a2d037a34d59c62bca31e8cf98
Damian Cyrana <damiancyrana@gmail.com>
2021-02-26 11:02:41 +0100

    Created folder for diskSpace project

---
+++ diskSpace/diskspace.ps1
@@ -0,0 +1,13 @@
+﻿$letter = 'c:'
+$valueToReturn = "TotalSpace"
+
+
+if($valueToReturn -eq "FreeSpace") {
+    Get-WmiObject win32_logicaldisk -Filter "DeviceId='$letter'" | Select FreeSpace
+} 
+elseif ($valueToReturn -eq "TotalSpace") {
+    Get-WmiObject win32_logicaldisk -Filter "DeviceId='$letter'" | Select Size
+}
+else {
+    Get-WmiObject win32_logicaldisk -Filter "DeviceId='$letter'" | Select @{n="Used"; e={$_.Size - $_.FreeSpace}}
+}
